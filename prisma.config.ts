df389ce2a1bdce35ae8a12a49e0a860f62f980e4
Andrija Vulicevic <vulicevic.andrija@gmail.com>
2026-02-02 18:41:16 +0100

    Prisma update

--- prisma.config.ts
+++ prisma.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     url:
       process.env["NODE"] !== "production"
         ? process.env["POSTGRES_URL_NON_POOLING"]
-        : process.env["POSTGRES_URL"],
+        : process.env["POSTGRES_PRISMA_URL"],
   },
 });
